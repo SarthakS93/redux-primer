51b876764924cf2c0dfa6c5f6fa0e2c1c3e36d5e
sarthak <sarthaks93@gmail.com>
2019-02-11 22:07:03 +0530

    added redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import './index.css';
@@ -11,10 +11,23 @@ import registerServiceWorker from './registerServiceWorker';
 
 const rootReducer = combineReducers({
     ctr: counterReducer,
-    res: resultReducer
+    res: resultReducer,
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+	return next => {
+		return action => {
+			console.log("Dispath middleware");
+			const result = next(action);
+			console.log("State is: ", store.getState());
+			return result;
+		};
+	};
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+	applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
